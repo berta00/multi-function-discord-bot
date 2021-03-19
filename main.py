65c467d849a52fefd527f7b50d705ae0de31c2e2
Marco Bertagnolli <62913693+Alienozzo@users.noreply.github.com>
2021-03-19 17:34:24 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-# PROGETTO SERIO
+import selenium
+from selenium import webdriver
+import simple_colors
+
+
+driver = webdriver.Firefox(executable_path="/home/alienozzo/Scrivania/Coding/aternos_bot/geckodriver")
+
+
+# interfaccia
+
+
+# selenium
+
+def connessioneAlServer(username, password):
