8a8e8725113ee652b06d20931a349bfdaeb39c76
maxglio <massimo.maglio.06@gmail.com>
2021-03-21 18:24:53 +0100

    interfaccia v1

--- main.py
+++ main.py
@@ -92,5 +92,4 @@ passwordServer = input("inserisci la password: ")         # implementala nella i
 
 connessioneAlServer(driver, nomeServer, passwordServer)   # implementala nella interfaccia
 accendiIlServer()                                         # implementala nella interfaccia
-#gggggg
 # esciDaAternos()                                         # implementala nella interfaccia
