3b1105c728ee2ed21920ff8bac5551bc4ddcac14
Alienozzo <marco2006bertagnolli@gmail.com>
2021-03-21 13:12:19 +0100

    selenium v1 - 5

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ driver = webdriver.Firefox(executable_path=str(locazioneDriver))
 os.system("clear")
 
 def connessioneAlServer(driver, username, password):
+    #connessione
     driver.get("https://aternos.org/go/")
     input1 = driver.find_element_by_xpath('//*[@id="user"]')
     input2 = driver.find_element_by_xpath('//*[@id="password"]')
@@ -25,24 +26,37 @@ def connessioneAlServer(driver, username, password):
     input2.send_keys(password)
     input3.click()
     time.sleep(1)
+    #controllo credenziali
     if driver.title == "Servers | Aternos | Free Minecraft Server":
         print()
-        print("loggato correttamente")
+        print(simple_colors.green("loggato correttamente"))
     elif driver.title == "Login or Sign up | Aternos | Free Minecraft Server":
+        os.system("clear")
+        print(simple_colors.red("hai sbagliato credenziali, rimettile:"))
         print()
-        print("hai sbagliato credenziali, rimettile:")
         nomeServer = input("inserisci lo username: ")
         passwordServer = input("inserisci la password: ")
         connessioneAlServer(driver, nomeServer, passwordServer)
+
+        esciDaAternos()
     else:
         print()
         print("qualcosa è andato storto!")
         esciDaAternos()
+    #entra nel server
     input4 = driver.find_element_by_xpath('/html/body/div/main/section/div/div[2]/div')
     input4.click()
     time.sleep(1)
     cookieBottone = driver.find_element_by_xpath('//*[@id="accept-choices"]')
     cookieBottone.click()
+    time.sleep(1)
+
+def accendiIlServer():
+    bottoneAvvio = driver.find_element_by_xpath('//*[@id="start"]')
+    bottoneAvvio.click()
+    time.sleep(0.5)
+    confermaNotifiche = driver.find_element_by_xpath('/html/body/div[2]/main/div/div/div/main/div/a[1]')
+    confermaNotifiche.click()
 
 def esciDaAternos():
     driver.quit()
@@ -55,5 +69,6 @@ nomeServer = input("inserisci lo username: ")             # implementala nella i
 passwordServer = input("inserisci la password: ")         # implementala nella interfaccia
 
 connessioneAlServer(driver, nomeServer, passwordServer)   # implementala nella interfaccia
+accendiIlServer()                                         # implementala nella interfaccia
 
 # esciDaAternos()                                         # implementala nella interfaccia
