11a825c262002e8c624880214fff193bddda3487
maxglio <massimo.maglio.06@gmail.com>
2021-03-22 14:59:57 +0100

    prova

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ os.system("pip install simple_colors")
 
 import simple_colors
 from sergio import *
-
+print("buongiorno")
+print ("hai scleto")
 nomeServer = input("inserisci lo username: ")             # implementala nella interfaccia
 passwordServer = input("inserisci la password: ")         # implementala nella interfaccia
 #marco gay
 connessioneAlServer(driver, nomeServer, passwordServer)   # implementala nella interfaccia
 accendiIlServer()                                         # implementala nella interfaccia
 # esciDaAternos()                                         # implementala nella interfaccia
-#marconano
-#ciao
-#ciao
+
