b7868b56220db43bc8811b64ff350ef3d00706ab
maxglio <massimo.maglio.06@gmail.com>
2021-03-22 15:09:21 +0100

    prova 2 simple_colors

--- main.py
+++ main.py
@@ -3,15 +3,16 @@ import os
 #controlla o installa le dipendenze
 os.system("pip install selenium")
 os.system("pip install simple_colors")
-
 import simple_colors
 from sergio import *
 print("buongiorno")
 print ("hai scleto")
+print ("Questo bot ha diverse funzionalità:")
 nomeServer = input("inserisci lo username: ")             # implementala nella interfaccia
 passwordServer = input("inserisci la password: ")         # implementala nella interfaccia
-#marco gay
+nocheat=input("questa funzione controlla che non vengano usati cheat ")
 connessioneAlServer(driver, nomeServer, passwordServer)   # implementala nella interfaccia
 accendiIlServer()                                         # implementala nella interfaccia
 # esciDaAternos()                                         # implementala nella interfaccia
+
 
